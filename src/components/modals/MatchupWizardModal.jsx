583c5512c018543bc82120726803412454b9c3b5
Andrew Rose <andrewlrose@hotmail.com>
2025-12-23 01:48:13 -0800

    Force overwrite: Matchup Wizard Platinum + Expert Intel

--- src/components/modals/MatchupWizardModal.jsx
+++ src/components/modals/MatchupWizardModal.jsx
@@ -7,7 +7,7 @@ const TEAM_LOGOS = {
   "Cardinals": "https://a.espncdn.com/i/teamlogos/nfl/500/ari.png", "Falcons": "https://a.espncdn.com/i/teamlogos/nfl/500/atl.png", "Ravens": "https://a.espncdn.com/i/teamlogos/nfl/500/bal.png", "Bills": "https://a.espncdn.com/i/teamlogos/nfl/500/buf.png", "Panthers": "https://a.espncdn.com/i/teamlogos/nfl/500/car.png", "Bears": "https://a.espncdn.com/i/teamlogos/nfl/500/chi.png", "Bengals": "https://a.espncdn.com/i/teamlogos/nfl/500/cin.png", "Browns": "https://a.espncdn.com/i/teamlogos/nfl/500/cle.png", "Cowboys": "https://a.espncdn.com/i/teamlogos/nfl/500/dal.png", "Broncos": "https://a.espncdn.com/i/teamlogos/nfl/500/den.png", "Lions": "https://a.espncdn.com/i/teamlogos/nfl/500/det.png", "Packers": "https://a.espncdn.com/i/teamlogos/nfl/500/gb.png", "Texans": "https://a.espncdn.com/i/teamlogos/nfl/500/hou.png", "Colts": "https://a.espncdn.com/i/teamlogos/nfl/500/ind.png", "Jaguars": "https://a.espncdn.com/i/teamlogos/nfl/500/jax.png", "Chiefs": "https://a.espncdn.com/i/teamlogos/nfl/500/kc.png", "Raiders": "https://a.espncdn.com/i/teamlogos/nfl/500/lv.png", "Chargers": "https://a.espncdn.com/i/teamlogos/nfl/500/lac.png", "Rams": "https://a.espncdn.com/i/teamlogos/nfl/500/lar.png", "Dolphins": "https://a.espncdn.com/i/teamlogos/nfl/500/mia.png", "Vikings": "https://a.espncdn.com/i/teamlogos/nfl/500/min.png", "Patriots": "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png", "Saints": "https://a.espncdn.com/i/teamlogos/nfl/500/no.png", "Giants": "https://a.espncdn.com/i/teamlogos/nfl/500/nyg.png", "Jets": "https://a.espncdn.com/i/teamlogos/nfl/500/nyj.png", "Eagles": "https://a.espncdn.com/i/teamlogos/nfl/500/phi.png", "Steelers": "https://a.espncdn.com/i/teamlogos/nfl/500/pit.png", "49ers": "https://a.espncdn.com/i/teamlogos/nfl/500/sf.png", "Seahawks": "https://a.espncdn.com/i/teamlogos/nfl/500/sea.png", "Buccaneers": "https://a.espncdn.com/i/teamlogos/nfl/500/tb.png", "Titans": "https://a.espncdn.com/i/teamlogos/nfl/500/ten.png", "Commanders": "https://a.espncdn.com/i/teamlogos/nfl/500/was.png", "Washington": "https://a.espncdn.com/i/teamlogos/nfl/500/was.png"
 };
 
-// --- 2. PICK ITEM COMPONENT (Restored) ---
+// --- 2. PICK ITEM COMPONENT (Restored from your file) ---
 const PickItem = ({ pick }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const hasRationale = pick.rationale && Array.isArray(pick.rationale) && pick.rationale.length > 0;
@@ -81,6 +81,7 @@ const RankBadge = ({ rank, type }) => {
 };
 
 // --- MAIN COMPONENT ---
+// 🔥 FIX: We now accept 'game' and 'stats' from App.jsx, AND optional currentWizardData
 export default function MatchupWizardModal({ isOpen, onClose, game, onBet, stats, currentWizardData }) {
   const [selectedBet, setSelectedBet] = useState(null);
 
